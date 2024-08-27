e5228e1ca83343e34fc2dab5944302124aded028
Guilherme Lessa <guilessa.irmd@gmail.com>
2024-08-27 14:33:17 -0300

    Alterações

--- src/main/java/jv/triersistemas/primeiro_projeto/controller/TarefaController.java
+++ src/main/java/jv/triersistemas/primeiro_projeto/controller/TarefaController.java
@@ -1,8 +1,12 @@
 package jv.triersistemas.primeiro_projeto.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,56 +15,40 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import jv.triersistemas.primeiro_projeto.Tarefa;
+
+import jv.triersistemas.primeiro_projeto.dto.TarefaDto;
+import jv.triersistemas.primeiro_projeto.service.TarefaService;
 
 @RestController
 @RequestMapping("/tarefas")
 public class TarefaController {
-	private static List<Tarefa> tarefas = new ArrayList<>();
-	private static long contador = 1;
 
-	@PostMapping
-	public Tarefa criarTarefa(@RequestBody Tarefa tarefa) {
-		tarefa.setId(contador++);
-		tarefas.add(tarefa);
-		return tarefa;
-	}
+	@Autowired
+	private TarefaService tarefaService;
 
 	@GetMapping
-	public List<Tarefa> listarTarefas() {
-		return tarefas;
+	public List<TarefaDto> getTodasTarefas() {
+		return tarefaService.getTodasTarefas();
 	}
 
 	@GetMapping("/{id}")
-	public Tarefa obterTarefa(@PathVariable Long id) {
-		for (Tarefa tarefa : tarefas) {
-			if (tarefa.getId().equals(id)) {
-				return tarefa;
-			}
-		}
-		return null;
+	public ResponseEntity<TarefaDto> getTarefaPorId(@PathVariable Long id) {
+		Optional<TarefaDto> tarefa = tarefaService.findById(id);
+		return tarefa.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body(null));
+	}
+
+	@PostMapping
+	public TarefaDto adicionarTarefa(@RequestBody TarefaDto novaTarefa) {
+		return tarefaService.adicionarTarefa(novaTarefa);
 	}
 
 	@PutMapping("/{id}")
-	public Tarefa atualizarTarefa(@PathVariable Long id, @RequestBody Tarefa novaTarefa) {
-		Tarefa tarefa = obterTarefa(id);
-		if (tarefa != null) {
-			tarefa.setTitulo(novaTarefa.getTitulo());
-			tarefa.setDescricao(novaTarefa.getDescricao());
-			tarefa.setCompleta(novaTarefa.isCompleta());
-			return tarefa;
-		}
-		return null;
+	public TarefaDto atualizarTarefa(@PathVariable Long id, @RequestBody TarefaDto tarefaAtualizada) {
+		return tarefaService.atualizarTarefa(id, tarefaAtualizada);
 	}
 
 	@DeleteMapping("/{id}")
-	public String deletarTarefa(@PathVariable Long id) {
-		for (Tarefa t : tarefas) {
-			if (t.getId().equals(id)) {
-				tarefas.remove(t);
-				return "Tarefa deletada com sucesso";
-			}
-		}
-		return "Tarefa não encontrada";
+	public void removerTarefa(@PathVariable Long id) {
+		tarefaService.removerTarefa(id);
 	}
 }
